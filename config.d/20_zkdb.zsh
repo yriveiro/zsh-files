b6f634ee8aaf8b05e558a900c9033a7981998f8c
Yago Riveiro <yago.riveiro@gmail.com>
2020-10-29 18:24:45 +0000

    [FIX] wrong behaviour on delete

--- config.d/20_zkdb.zsh
+++ config.d/20_zkdb.zsh
@@ -1,7 +1,7 @@
 typeset -g -A key
 
 # Default key bindings
-bindkey "^[[3~" backward-delete-word
+bindkey "^[[3~" delete-word
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
 bindkey "^[[1;2H" backward-word
