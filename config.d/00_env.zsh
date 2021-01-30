c983a1f23ea3a0a79532b57f246b89554870e584
Yago Riveiro <yago.riveiro@gmail.com>
2021-01-30 12:00:55 +0000

    [FEAT] cleanup done in aliases

--- config.d/00_env.zsh
+++ config.d/00_env.zsh
@@ -6,13 +6,17 @@ export EDITOR=nvim
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export RUST_SRC_PATH="/usr/local/src/rustc/src"
   export PATH=$PATH:/usr/local/bin/go
-  export GOPATH=~/Development/go
+  export GOPATH=~/Development/go/bin
   export CLOUDSDK_PYTHON=/usr/local/Cellar/python@3.8/3.8.6_1/bin/python3.8
 fi
 
 if [[ "$OSTYPE" == "linux"* ]]; then
-  export PATH=$PATH:/usr/local/go/bin:/home/yriveiro/Development/go/bin
-  export GOPATH=~/Development/go
+  export PATH=$PATH:/usr/local/go/bin:/data/Development/go/bin:/usr/local/kubebuilder/bin
+  export GOPATH=/data/Development/go
+
+  if [[ -d /usr/local/kubebuilder/bin ]]; then
+    export PATH=$PATH:/usr/local/kubebuilder/bin
+  fi
 fi
 
 export GPG_TTY=$(tty)
--- config.d/99_aliases.zsh
+++ config.d/99_aliases.zsh
@@ -1,12 +1,25 @@
 alias __="nvim"
 alias vim="nvim"
-alias github="~/Development/github"
-alias gist="~/Development/gist"
-alias aqom="~/Development/aqom"
-alias brewup="brew update && brew upgrade && brew cleanup"
 alias gitstamp="cat ~/.localgit.cfg >> .git/config"
 
-alias l="exa -1"
-alias ll="exa -l"
-alias lll="exa -l -L=2 -T"
-alias kc="kubectl"
+alias l="exa -l"
+alias ll="exa -l -L=2 -T"
+alias lll="exa -l -L=3 -T"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  alias github="~/Development/github"
+  alias gist="~/Development/gist"
+  alias brewup="brew update && brew upgrade && brew cleanup"
+  alias devel="cd ~/Development"
+  alias kc="kubectl"
+fi
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+  alias github="cd /data/Development/github"
+  alias gist="cd /data/Development/gist"
+  alias devel="cd /data/Development"
+
+  # microk8s aliases
+  alias mc='microk8s kubectl'
+  alias mcg='microk8s kubectl get'
+fi
