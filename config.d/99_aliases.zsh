6caab49356d7c13b4e5016f4238a1365cd2d3251
Yago Riveiro <yago.riveiro@gmail.com>
2022-10-15 21:21:48 +0100

    feat: add aliases for git worktree

--- config.d/99_aliases.zsh
+++ config.d/99_aliases.zsh
@@ -31,6 +31,8 @@ alias gps="git push"
 alias gap="git ap ."
 alias gst="git st"
 alias gci="git ci -S -m "
+alias gw="git worktree"
+alias gwl="git worktree list"
 
 if command kubectl 2>&1 > /dev/null; then
   alias k="kubectl"
