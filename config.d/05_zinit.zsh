d31c31867be7f0bfda178f250d74333f08d4abf1
Yago Riveiro <yago.riveiro@gmail.com>
2022-10-30 22:36:38 +0000

    chore(plugins): remove pass add z and vi mode plugins

--- config.d/05_zinit.zsh
+++ config.d/05_zinit.zsh
@@ -3,12 +3,14 @@
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit wait lucid light-mode for \
+    jeffreytse/zsh-vi-mode \
     zdharma-continuum/z-a-rust \
     zdharma-continuum/z-a-as-monitor \
     zdharma-continuum/z-a-patch-dl \
     zdharma-continuum/z-a-bin-gem-node \
     zsh-users/zsh-history-substring-search \
     Dbz/kube-aliases \
+    agkozak/zsh-z \
   atinit"autoload -Uz bashcompinit && bashcompinit" \
     'https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion' \
   atinit"zicompinit; zicdreplay" \
@@ -19,7 +21,6 @@ zinit wait lucid light-mode for \
       zsh-users/zsh-completions \
       yriveiro/zsh-completions \
   as"completion" \
-      OMZP::pass/_pass \
       OMZP::aws/aws.plugin.zsh \
       dwaynebradley/k3d-oh-my-zsh-plugin \
       MenkeTechnologies/zsh-cargo-completion \
