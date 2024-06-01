7885727846b2944908716135ff7461c719a4f910
Yago Riveiro <yago.riveiro@gmail.com>
2024-06-01 14:38:56 +0100

    feat: git worktree func
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- config.d/99_git.zsh
+++ config.d/99_git.zsh
@@ -1,3 +1,18 @@
+# ANSI color codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+RESET="\033[0m"
+
+# Symbols
+CROSS_MARK="❌"
+CHECK_MARK="✅"
+WORKING="🛠️"
+ROCKET="🚀"
+
+CROSS="${RED}${CROSS_MARK}${RESET}"
+CHECK="${GREEN}${CHECK_MARK}${RESET}"
+
 # Clones a Git repository with customizable service host and protocol.
 # Usage: cloneGitRepo <repository_path> [service_host] [protocol]
 function gcb() {
@@ -6,12 +21,12 @@ function gcb() {
   local protocol=${3:-ssh}
 
   if [[ -z "$repository_path" ]]; then 
-    echo "${RED}${CROSS_MARK}${RESET} Repository path is required."
+    echo "${CROSS} Repository path is required."
     return 1
   fi
 
   if [[ "$protocol" != "ssh" && "$protocol" != "https" ]]; then 
-    echo "${RED}${CROSS_MARK}${RESET} Invalid protocol, use ssh or https."
+    echo "${CROSS} Invalid protocol, use ssh or https."
     return 1;
   fi
 
@@ -24,41 +39,55 @@ function gcb() {
 
   local repository="${repository_path##*/}"
 
-  git clone --bare "${url}" "${repository}"
+  git clone --quiet --bare "${url}" "${repository}"
+
+
+  echo "${CHECK} Cloned ${YELLOW}${repository_path}${RESET} to ${YELLOW}${repository}${RESET}."
 
   if [ $? -ne 0 ]; then
-    echo "${RED}${CROSS_MARK}${RESET} Failed to clone repository."
+    echo "${CROSS} Failed to clone ${YELLOW}${repository}${RESET}."
     return 1
   fi
 
-  cd "${repository}" || {echo "${RED}${CROSS_MARK}${RESET} Failed to enter ${repository}."; return 1;}
+  cd "${repository}" || {echo "${CROSS} Failed to enter ${YELLOW}${repository}${RESET}."; return 1;}
 
   # Check if the 'main' branch exists, fall back to 'master' if not
   local branch_name="main"
+
   if ! git show-ref --verify --quiet "refs/heads/${branch_name}"; then
     branch_name="master"
 
     if ! git show-ref --verify --quiet "refs/heads/${branch_name}"; then
-      echo "${RED}${CROSS_MARK}${RESET} Neither 'main' nor 'master' branches found."
+      echo "${CROSS} Neither 'main' nor 'master' branches found."
       return 1
     fi
   fi
 
-  git worktree add "$branch_name" || { echo "${RED}${CROSS_MARK}${RESET} Failed to add worktree for main."; return 1; }
+  git worktree add -q "${branch_name}" || { echo "${CROSS} Failed to add worktree for main."; return 1; }
+
+  cd "${branch_name}"
+
+  git config advice.setUpstreamFailure false
   git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 
-  echo "${GREEN}${CHECK_MARK}${RESET} Cloned ${repository_path} to ${repository}"
+  echo "${CHECK} ${YELLOW}${branch_name}${RESET} ready to track ${YELLOW}origin/${branch_name}${RESET}."
+
+  git fetch -q origin &> /dev/null
+  git branch -q --set-upstream-to=origin/"${branch_name}" "${branch_name}"
+
+  echo "${ROCKET} ${YELLOW}${repository}${RESET} ready to use."
 }
 
 function gwa() {
   local branch=${1}
 
   if [[ -z "${branch}" ]]; then
-    echo "${RED}${CROSS_MARK}${RESET} Worktree name is required."
+    echo "${CROSS} Worktree name is required."
   fi
 
-  git worktree add "${branch}" || { echo "${RED}${CROSS_MARK}${RESET} Failed to add worktree."; return 1; }
+  git worktree add -q "${branch}" || { echo "${CROSS} Failed to add worktree."; return 1; }
 
+  echo "${CHECK} ${YELLOW}${branch}${RESET} ready to use."
 
   cd "${branch}"
 }
